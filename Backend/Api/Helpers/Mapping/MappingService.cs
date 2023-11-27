56bf6fcc061bc37a4622cfaa7a30d354bca29b03
Dan Stjärnborg <dan.stjarnborg@gmail.com>
2023-11-27 11:48:55 +0100

    Base for Repositories and Tests

---
+++ Backend/Api/Helpers/Mapping/MappingService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Backend.Dtos;
+using Entity.Models;
+
+namespace Api.Helpers.Mapping;
+
+public class MappingService : Profile
+{
+
+    public MappingService()
+    {
+        CreateMap<StudentModel, StudentDto>().ReverseMap();
+        CreateMap<CompanyModel, CompanyDto>().ReverseMap();
+
+    }
+}
