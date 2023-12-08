10908181afa3687712743b52af2fe930907203a5
Dan Stjärnborg <dan.stjarnborg@gmail.com>
2023-12-08 07:51:03 +0100

    Adjustments to Home Added Styling in Home.css Built new Carousel Component (not working) - Progress later.
    Installed Slick, did not get it to work.
    Will try later.

--- Frontend/src/Pages/Home.tsx
+++ Frontend/src/Pages/Home.tsx
@@ -1,5 +1,6 @@
-import { Row, Col, Card, Typography } from "antd";
+import { Typography } from "antd";
 import { Container } from "react-bootstrap";
+import  Carousel from "../Components/Carousel"
 import "../Styles/homes.css";
 import heroBannerImage from "../assets/herobanner.jpg"; // Replace with the actual path to your image
 
@@ -8,34 +9,23 @@ const { Title, Paragraph } = Typography;
 export function Home() {
   return (
     <>
-      <div className="hero-banner" style={{ color: "white", height: "50vh", backgroundImage: `url(${heroBannerImage})` }}>
+      <div className="hero-banner" style={{ height: "50vh", backgroundImage: `url(${heroBannerImage})` }}>
+      <div className="overlay"></div>
         <Container>
-          <Title level={1}>Hero Banner Title</Title>
-          <Paragraph>Some text over the picture</Paragraph>
+          <Title style={{ color: "black" }} level={1}>SkillSync</Title>
+          <Paragraph  className="lead" style={{ color: "black"}}> <b>the revolutionary matching tool designed to seamlessly connect aspiring 
+          software engineers with the opportunities that align with their passions and expertise. 
+          Imagine a platform where students can effortlessly bridge the gap between their academic knowledge and real-world application, 
+          all while receiving mentorship, gaining valuable internship experience, and even securing potential job opportunities. 
+          SkillSync goes beyond traditional job boards by intricately analyzing a user's skills, interests, and educational background, 
+          offering a curated selection of companies eager to provide mentorship and hands-on experience. By facilitating this symbiotic relationship, 
+          SkillSync transforms the job search experience into a tailored journey, ensuring that students not only find opportunities but also thrive in 
+          environments that foster growth. Elevate your career prospects with<br /> <br / >SkillSync – where your skills meet their match!</b></Paragraph>
         </Container>
       </div>
 
-      <div className="grey-background" style={{ height: "90vh" }}>
-        <Container>
-          <Row gutter={[16, 16]} justify="center">
-            <Col xs={24} sm={12} md={8} lg={6}>
-              <Card className="custom-card" style={{ height: "40vh" }} title="Card 1">
-                Content for Card 1
-              </Card>
-            </Col>
-            <Col xs={24} sm={12} md={8} lg={6}>
-              <Card className="custom-card" style={{ height: "40vh" }} title="Card 2">
-                Content for Card 2
-              </Card>
-            </Col>
-            <Col xs={24} sm={12} md={8} lg={6}>
-              <Card className="custom-card" style={{ height: "40vh" }} title="Card 3">
-                Content for Card 3
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+       <Carousel />
+      
 
       <div className="grey-green-background" style={{ height: "90vh" }}>
         <Container>
